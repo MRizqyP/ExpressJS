bc741aa07949825a93f50277159fd84096c656f5
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-13 14:08:44 +0700

    berhasil semuanya

--- routes/books.js
+++ routes/books.js
@@ -84,37 +84,78 @@ module.exports = function(app) {
     }
   );
   /* update todo. */
-  app.put("/books/:id", function(req, res, next) {
-    const book_id = req.params.id;
-    const { title, description } = req.body;
-    model.book
-      .update(
-        {
-          title: title,
-          description: description
-        },
-        {
-          where: {
-            id: book_id
+  app.put(
+    "/books/:id",
+    [
+      check("title")
+        .notEmpty()
+        .withMessage("Title tidak boleh kosong")
+        .isString(),
+      check("author")
+        .notEmpty()
+        .withMessage("Author tidak boleh kosong")
+        .isString(),
+      check("published_date")
+        .isBefore()
+        .withMessage("Tanggal tidak mungkin lebih dari tanggal sekarang")
+        .notEmpty(),
+      check("pages")
+        .isNumeric()
+        .notEmpty(),
+      check("language")
+        .isString()
+        .notEmpty(),
+      check("published_id")
+        .isString()
+        .notEmpty()
+    ],
+    function(req, res, next) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+      const book_id = req.params.id;
+      const {
+        title,
+        author,
+        published_date,
+        pages,
+        language,
+        published_id
+      } = req.body;
+      model.book
+        .update(
+          {
+            title: title,
+            author: author,
+            published_date: published_date,
+            pages: pages,
+            language: language,
+            published_id: published_id
+          },
+          {
+            where: {
+              id: book_id
+            }
           }
-        }
-      )
-      .then(book =>
-        res.json({
-          error: false,
-          message: "Buku berhasil di update."
-        })
-      )
-      .catch(error =>
-        res.json({
-          error: true,
-          error: error
-        })
-      );
-  });
+        )
+        .then(book =>
+          res.json({
+            error: false,
+            message: "Buku berhasil di update."
+          })
+        )
+        .catch(error =>
+          res.json({
+            error: true,
+            error: error
+          })
+        );
+    }
+  );
   /* GET todo listing. */
   /* Delete todo. */
-  app.delete("books/:id", function(req, res, next) {
+  app.delete("/books/:id", function(req, res, next) {
     const book_id = req.params.id;
     model.book
       .destroy({
