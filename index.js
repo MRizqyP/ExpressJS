3cee22d0243c93ff3bdc4be84be59f035005f0c2
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-13 11:57:12 +0700

    berhasil pust,get

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+require("./routes/books.js")(app);
+
+var server = app.listen(3001, "127.0.0.1", function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("App Listening at http://%s:%s", host, port);
+});
--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,107 @@
 var model = require("../models/index");
 
-module.exports = function(app) {};
+module.exports = function(app) {
+  app.get("/books", function(req, res, next) {
+    model.book
+      .findAll({})
+      .then(books =>
+        res.json({
+          error: false,
+          data: books
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          data: [],
+          error: error
+        })
+      );
+  });
+
+  app.post("/books", function(req, res, next) {
+    const {
+      title,
+      author,
+      published_date,
+      pages,
+      language,
+      published_id
+    } = req.body;
+    model.book
+      .create({
+        title: title,
+        author: author,
+        published_date: published_date,
+        pages: pages,
+        language: language,
+        published_id: published_id
+      })
+      .then(book =>
+        res.status(201).json({
+          error: false,
+          data: book,
+          message: "Buku baru berhasil di tambahkan."
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          data: [],
+          error: error
+        })
+      );
+  });
+  /* update todo. */
+  app.put("/books/:id", function(req, res, next) {
+    const book_id = req.params.id;
+    const { title, description } = req.body;
+    model.book
+      .update(
+        {
+          title: title,
+          description: description
+        },
+        {
+          where: {
+            id: book_id
+          }
+        }
+      )
+      .then(book =>
+        res.json({
+          error: false,
+          message: "todo has been updated."
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          error: error
+        })
+      );
+  });
+  /* GET todo listing. */
+  /* Delete todo. */
+  app.delete("books/:id", function(req, res, next) {
+    const book_id = req.params.id;
+    model.book
+      .destroy({
+        where: {
+          id: book_id
+        }
+      })
+      .then(status =>
+        res.json({
+          error: false,
+          message: "todo has been delete."
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          error: error
+        })
+      );
+  });
+};
