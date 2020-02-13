b8c7e1bcd951196e77a4d622beea34cd6b5401ab
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-13 13:43:24 +0700

    tambah validasi express

--- routes/books.js
+++ routes/books.js
@@ -1,4 +1,5 @@
 var model = require("../models/index");
+const { check, validationResult } = require("express-validator");
 
 module.exports = function(app) {
   app.get("/books", function(req, res, next) {
@@ -19,39 +20,69 @@ module.exports = function(app) {
       );
   });
 
-  app.post("/books", function(req, res, next) {
-    const {
-      title,
-      author,
-      published_date,
-      pages,
-      language,
-      published_id
-    } = req.body;
-    model.book
-      .create({
-        title: title,
-        author: author,
-        published_date: published_date,
-        pages: pages,
-        language: language,
-        published_id: published_id
-      })
-      .then(book =>
-        res.status(201).json({
-          error: false,
-          data: book,
-          message: "Buku baru berhasil di tambahkan."
+  app.post(
+    "/books",
+    [
+      check("title")
+        .notEmpty()
+        .withMessage("Title tidak boleh kosong")
+        .isString(),
+      check("author")
+        .notEmpty()
+        .withMessage("Author tidak boleh kosong")
+        .isString(),
+      check("published_date")
+        .isBefore()
+        .withMessage("Tanggal tidak mungkin lebih dari tanggal sekarang")
+        .notEmpty(),
+      check("pages")
+        .isNumeric()
+        .notEmpty(),
+      check("language")
+        .isString()
+        .notEmpty(),
+      check("published_id")
+        .isString()
+        .notEmpty()
+    ],
+    function(req, res, next) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+      const {
+        title,
+        author,
+        published_date,
+        pages,
+        language,
+        published_id
+      } = req.body;
+      model.book
+        .create({
+          title: title,
+          author: author,
+          published_date: published_date,
+          pages: pages,
+          language: language,
+          published_id: published_id
         })
-      )
-      .catch(error =>
-        res.json({
-          error: true,
-          data: [],
-          error: error
-        })
-      );
-  });
+        .then(book =>
+          res.status(201).json({
+            error: false,
+            data: book,
+            message: "Buku baru berhasil di tambahkan."
+          })
+        )
+        .catch(error =>
+          res.json({
+            error: true,
+            data: [],
+            error: error
+          })
+        );
+    }
+  );
   /* update todo. */
   app.put("/books/:id", function(req, res, next) {
     const book_id = req.params.id;
